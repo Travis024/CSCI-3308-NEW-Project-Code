01ebc7bc1e6dfdafb11a9f36ba17530e91184b91
Matthew Waymouth <matthewwaymouth@rgnt2-64-237-dhcp.int.colorado.edu>
2019-04-08 13:09:31 -0600

    Updated __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -3,26 +3,13 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate
 from flask_login import LoginManager
-#from models import User
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# app.config['DEBUG'] = True
-#
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-# %(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-# db.init_app(app)
-
 login = LoginManager(app)
 login.login_view = 'login'
 
 from app import routes, models
-
-
-
-#
-# if __name__ == "__main__":
-#     app.run()
